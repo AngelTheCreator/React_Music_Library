e06648aeda819c6114eb7f9d24e6d0b3df27ee82
AngelTheCreator <angel_guevara94@hotmail.com>
2022-07-05 16:32:51 -0700

    search functionality and context implemented

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,11 +1,17 @@
 import {useState} from 'react'
     function SearchBar(props){
-        let [searchTerm, setSearchTerm] = useState('')
+        //let [searchTerm, setSearchTerm] = useState('')
 
             return (
                 <form>
-                    <input type='text' placeholder='Enter a search term here' />
+                    
+                    <input type='text' placeholder='Enter a search term here'
+                    onChange={
+                        (e) => props.handleSearch(e, e.target.value)
+                        } />
+                    
                     <input type='submit' />
+               
                 </form>
             )
 }
